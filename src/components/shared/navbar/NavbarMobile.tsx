4d742a7dfecfa1da75057a3c3d094f2c2ff96c41
simplyratl <nikicaraznatovic123@gmail.com>
2023-08-12 22:25:06 +0200

    adding quiz page and fixing signout on mobile

--- src/components/shared/navbar/NavbarMobile.tsx
+++ src/components/shared/navbar/NavbarMobile.tsx
@@ -1,42 +1,46 @@
 "use client";
 
-import React from "react";
-import { motion } from "framer-motion";
-import Link from "next/link";
-import { Button } from "@/components/ui/button";
 import { navLinks } from "@/app/(constants)/constants";
+import { Button } from "@/components/ui/button";
+import { motion } from "framer-motion";
+import { signOut } from "next-auth/react";
+import Link from "next/link";
+import React from "react";
 
 type Props = {
   setOpen: React.Dispatch<React.SetStateAction<boolean>>;
 };
 
 const NavbarMobile = ({ setOpen }: Props) => {
-  const navLinksMobile = [
-    ...navLinks,
-
-    {
-      name: "Sign out",
-      href: "/api/auth/signout",
-    },
-  ];
   return (
     <motion.div
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
-      className="fixed inset-0 z-10 h-screen bg-background"
+      className="fixed inset-0 z-10 h-screen overflow-y-auto bg-background"
     >
       <div className="relative flex h-full items-center justify-center">
-        <ul className="space-y-4">
-          {navLinksMobile.map((link) => (
+        <ul className="space-y-3">
+          {navLinks.map((link) => (
             <li key={link.name}>
-              <Link href={link.href}>
-                <Button variant="link" className="poppins m-0 p-0 text-[8vw]">
+              <Link href={link.href} onClick={() => setOpen(false)}>
+                <Button variant="link" className="poppins m-0 p-0 text-4xl">
                   {link.name}
                 </Button>
               </Link>
             </li>
           ))}
+          <li>
+            <div>
+              <Button
+                onClick={() => signOut()}
+                variant="link"
+                className="poppins m-0 p-0 text-4xl"
+              >
+                Sign out
+              </Button>
+            </div>
+          </li>
         </ul>
       </div>
     </motion.div>
